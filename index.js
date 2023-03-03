b63b366a6e52fe9ec3ab74cfe5cfa58af39522ba
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-03-03 16:33:44 -0500

    Loop Lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let inames = []
+    for (let i = 0; i < names.length; i++ ){
+        inames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return inames
+};
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number--)
+    }
+};
